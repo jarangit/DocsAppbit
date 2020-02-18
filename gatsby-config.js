ff4af865d7a9caa0c1eba9253df3bda27f367960
jarangit <52028847+jarangit@users.noreply.github.com>
2020-02-18 16:11:28 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       options: {
         typeName: `WordPress`,
         fieldName: `wordPress`,
-        url: `http://docs.dev-app-bit.com`,
+        url: `http://docs.dev-app-bit.com/graphql`,
         // refetchInterval:60
        },
       },
