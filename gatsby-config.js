ad60a2a93b17916ca88c5e2b0582134f1e1bd760
jarangit <jaran.dch@gmail.com>
2020-02-21 10:47:02 +0700

    E sc menuleft.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+require("dotenv").config({
+  path: '.env'
+})
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -11,7 +14,7 @@ module.exports = {
       options: {
         typeName: `WordPress`,
         fieldName: `wordPress`,
-        url: `http://docs.dev-app-bit.com/graphql`,
+        url: process.env.API_URL_DEVELOP,
         // refetchInterval:60
        },
       },
