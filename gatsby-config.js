863661032eb93f7300a6e014c2ba4d3f59630aeb
jarangit <jaran.dch@gmail.com>
2020-02-21 10:48:30 +0700

    e sc menuleft.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
       options: {
         typeName: `WordPress`,
         fieldName: `wordPress`,
-        url: process.env.API_URL_DEVELOP,
+        url: process.env.API_URL_PRODUCTION,
         // refetchInterval:60
        },
       },
